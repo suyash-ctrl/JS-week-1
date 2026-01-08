306367754dbd2c0ddb9dcd0a3b874373ddd8b62d
Suyash <suyashdogra@gmail.com>
2026-01-09 01:24:01 +0530

    Day1:Basics

--- day1.js
+++ day1.js
@@ -4,3 +4,23 @@ console.log("My name is "+ myName);
 var x= Symbol("hii");
 console.log(x);
 console.log(String(x));
+
+function addSquare (a,b){
+    function square (x){
+        return x*x;
+    }
+    return (square (a)+ square(b));
+
+}
+console.log("Sum of squares of 4 and 5 is: ",addSquare(4,5));
+
+var st1 = "My name is Chiku";
+var st2 = "Ohayo Sekai Good morning worldddd";
+
+console.log(st1.charAt(6));
+console.log(st1.charCodeAt(6));
+console.log(st1.concat(st2));
+console.log(st1);
+console.log(st1.endsWith("Chiku"));
+console.log(st1.endsWith("u"));
+console.log(st2.slice(0,4));

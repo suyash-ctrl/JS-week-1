d7da1cecb137ee8623b5d4d66eb02e9d4e1d63a5
Suyash <suyashdogra@gmail.com>
2026-01-09 18:20:54 +0530

    learnt searching dom

---
+++ day2-dom/app.js
@@ -0,0 +1,17 @@
+// DOM Manipulation
+
+// getElementById()
+const head = document.getElementById('title');
+console.log(head);
+
+// getElementsByClassName()
+
+
+// getElementsByTagName()
+
+
+// querySelector()
+
+
+// querySelectorAll()
+
